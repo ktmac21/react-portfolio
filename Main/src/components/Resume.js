b260a2e0deff87ee4826974a3c89d468f2adef8f
Katie Eagles <kteagles21@gmail.com>
2021-10-23 14:20:17 -0400

    added media queries

--- Main/src/components/Resume.js
+++ Main/src/components/Resume.js
@@ -13,14 +13,14 @@ const Resume = () => {
             <div className='container3'>
                 <div className='card2'>
                     <h2>Education</h2>
-                    <img src={penn} alt='penn' />
+                    <img className='logos'src={penn} alt='penn' />
                     <ul className='flex1 experience'>
                         <li>University of Penn's</li>
                         <li>LPS Coding Bootcamp</li>
                         <li>Full Stack Web Development</li>
                         <br/>
                     </ul>
-                    <img src={bloom} alt='bloom' />
+                    <img className='logos' src={bloom} alt='bloom' />
                     <ul className='flex1 experience'>
                         <li>Bloomsburg University</li>
                         <li>Bachelor of Arts in Criminal Justice</li>
@@ -30,7 +30,7 @@ const Resume = () => {
 
                 <div className='card2'>
                     <h2>Experience</h2>
-                    <img src={nationwide} alt='nationwide' />
+                    <img className='logos'src={nationwide} alt='nationwide' />
                     <ul className='flex1 experience'>
                         <li>Nationwide Insurance</li>
                         <li>10 years Commercial Underwriting</li>
@@ -39,7 +39,7 @@ const Resume = () => {
                 </div>
                 <div className='card2'>
                     <h2>Resume</h2>
-                    <a className='resume' href={resume} download="EaglesResume.pdf">Download here</a>
+                    <a className='resume flex1' href={resume} download="EaglesResume.pdf">Download here</a>
                     <img className='resumepic' src={icon} alt='resume' width='200' height='200' />
                 </div>
             </div>
--- Main/src/components/Skills.js
+++ Main/src/components/Skills.js
@@ -2,16 +2,14 @@ const Skills = () => {
     return (
         <div className='flex2'>
         <h1 className='skills'>Skills</h1>
-        <div>
-
-        
+        <div className='flex2'>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width='75' height='75' alt='javascript' />
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width='75' height='75' alt='html'/>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width='75' height='75' alt='css'/>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" width='75' height='75' alt='node'/>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" width='75' height='75' alt='react' />
 
-        <div>
+        <div className='flex2'>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" width='75' height='75' alt='mysql'/>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" width='75' height='75' alt='mongo'/>
             <img className='icons' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" width='75' height='75' alt='github'/>
