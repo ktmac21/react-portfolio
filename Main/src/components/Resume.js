2f614c3c8172d26cecf4a066a1d5c76299846ca3
Katie Eagles <kteagles21@gmail.com>
2021-11-22 16:27:00 -0500

    edited resume component

--- Main/src/components/Resume.js
+++ Main/src/components/Resume.js
@@ -9,37 +9,52 @@ const Resume = () => {
     return (
 
         <div>
-            <h1>Experience</h1>
+            <h2>Education</h2>
             <div className='container3'>
                 
                 <div className='card2'>
-                    <h2>Education</h2>
+                    
                     <img className='logos'src={penn} alt='penn' />
                     <h3> University of Pennsylvania</h3>
-                    <p>Full Stack Web Development Bootcamp</p>
-                    <img className='logos' src={bloom} alt='bloom' />
-                    <h3>Bloomsburg University</h3>
-                    <p>Bachelor's of Art in Criminal Justice</p>
-                    <p>Cume Laude 2003</p>
+                    <p>Full Stack Web Development Certificate</p>
+                    <p>November 13, 2021</p>
+                    
                     
                 </div>
 
                 <div className='card2'>
-                    <h2>Experience</h2>
+                    <img className='logos' src={bloom} alt='bloom'/>
+                    <h3>Bloomsburg University</h3>
+                    <p>Bachelor's of Art in Criminal Justice</p>
+                    <p>Cume Laude</p>
+                    
+                </div>
+            </div>
+            <h2>Experience</h2>
+            <div className='container3'>
+
+            
+
+                <div className='card2'>
+                    
                     <img className='logos'src={nationwide} alt='nationwide' />
                     <h3>Nationwide Insurance Company</h3>
                     <p>Commercial Underwriting</p>
                     
                     
                 </div>
+
+                
                 
                 <div className='card2'>
-                    <h2>Resume</h2>
-                    <a className='resume' href={resume} download="EaglesResume.pdf"><img className='resumepic' src={icon} alt='resume' width='200' height='200' /></a>
-                    
+               
+                    <a href={resume} download="EaglesResume.pdf"><img className='resumepic' src={icon} alt='resume'/></a>
+                    <h3>Download Resume</h3>
                 </div>
+                
             </div>
-        </div>
+            </div>
+        
     )
 }
 
