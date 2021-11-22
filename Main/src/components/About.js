791f8136ba0fa5ec89a67ca3dcbae680060c7b96
Katie Eagles <kteagles21@gmail.com>
2021-11-22 14:57:59 -0500

    updated resume and banner heigheight

--- Main/src/components/About.js
+++ Main/src/components/About.js
@@ -10,9 +10,8 @@ const About = () => {
             <br />
             
             <p className='aboutme'> Welcome to my portfolio page! I am from Perkasie, Pennsylvania which is right 
-                outside of Philadelphia. I am currently a student of University of Penn's Full Stack 
-                Web Development coding bootcamp. I am very excited about making this transition into the challenging 
-                and exciting world of technology. I approach any kind of change as an opportunity to grow. 
+                outside of Philadelphia. I am a recent graduate of University of Penn's Full Stack 
+                Web Development coding bootcamp. I am very excited about making this transition into technology. I approach any kind of change as an opportunity to grow. 
                 I love coding and learning new technologies. I will never stop learning and challenging myself. 
                 Take a look at my current projects below. If you want to learn more please don't hesitate to contact me.</p>
             
