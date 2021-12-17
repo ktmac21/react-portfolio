fb430a77d655553d96615fa71c8b19acb1ef3a14
Katie Eagles <kteagles21@gmail.com>
2021-12-16 19:21:23 -0500

    deploying

--- Main/src/components/Navbar.js
+++ Main/src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <Link to='/about'>About</Link>
                 <Link to='/projects'>Projects</Link>
                 <Link to='/resume'>Resume</Link>
-                <Link to='/contact'>Contacts</Link>
+                <Link to='/contact'>Contact</Link>
                 
                 
                 
